0d2eccd96c828210022b3d3d4804b10a123248f2
Wylan Neely <wylanneely@gmail.com>
2022-08-27 22:47:20 -0700

    LandingVC
    outlets and actions
    prepared to implement saving with userdefaults

---
+++ LandingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LandingPageViewController.swift
+//  AlphabetVisionABCs
+//
+//  Created by Wylan L Neely on 8/27/22.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        self.viewDidLoad()
+        
+        
+    }
+
+
+
+
+    @IBOutlet weak var newGameButton: UIButton!
+    
+    @IBOutlet weak var continueButton: UIButton!
+    
+    @IBAction func continueTapped(_ sender: Any) {
+    }
+    
+    @IBAction func newGameTapped(_ sender: Any) {
+    }
+    
+}
